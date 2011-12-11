6502ad4bb9c4d3ab01df96c7f8667cb66e8275e7
Nils Hitze <nhitze@gmail.com>
2011-12-11 11:06:58 +0100

    blafasel

---
+++ gplus2rss.php
@@ -0,0 +1,61 @@
+<?php
+error_reporting(E_ALL);
+$cacheFile = './cache/gplus-cache';
+$cacheLoaded = false;
+exit;
+if(!file_exists($cacheFile)) {
+	$handle = fopen($cacheFile, 'a');
+	fclose($handle);
+	if(!$handle) {
+		echo 'cache failed';
+	}
+} else {
+	if((time() - filemtime($cacheFile) < 1800)
+	&& filesize($cacheFile)>0) {
+		$content = file_get_contents($cacheFile);
+		$cacheLoaded = true;
+	}
+}
+
+if($cacheLoaded == false) {
+
+	$key = 'AIzaSyAKLOkZ5kzTfGszyz4Hro0kwzZwkWQOG0E';
+	$url = 'https://www.googleapis.com/plus/v1/people/113799277735885972934/activities/public?alt=json&pp=1&key=';
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url.$key);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$content = curl_exec($ch);
+	curl_close($ch);
+//	file_put_contents($cacheFile, $content);
+}
+$gplus = json_decode($content);
+?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<rss version="2.00">
+<channel>
+<title>Mein GPlus</title>
+<link>http://plus.google.com</link>
+<description></description>
+<language>de-de</language>
+<?php
+require_once 'simplepie.inc';
+
+$feed = new SimplePie();
+$feed->set_feed_url('http://silberkind.de/gplus.php');
+$feed->enable_cache(false);
+$feed->init();
+
+foreach($gplus->items as $item) { ?>
+	<item>
+	<title><?php echo $item->title; ?></title>
+	<link><?php echo $item->url; ?></link>
+	<description>
+		<?php echo $item->object->content ?>
+	</description>
+	</item>
+<?php } ?>
+
+</channel>
+</rss>
