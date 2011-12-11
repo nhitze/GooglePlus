5a0e74a2543eba77cac24f281c68373e27d51754
Andreas Gohr <andi@splitbrain.org>
2011-12-11 13:51:55 +0100

    added more RSS fields
    now proper dates and GUIDs are set and enclosures are added

--- gplus2rss.php
+++ gplus2rss.php
@@ -30,25 +30,42 @@ if(!$content) die('Failed to load G+ data');
 $gplus = json_decode($content);
 if(!$gplus) die('Failed to decode G+ data');
 
+// for debugging:
+#print_r($gplus); exit;
 
-?>
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<rss version="2.00">
-<channel>
-<title>Google+ Feed:</title>
-<link>http://plus.google.com/<?php echo $googlePlusID ?></link>
-<description></description>
-<language>de-de</language>
-<?php
-foreach($gplus->items as $item) { ?>
-    <item>
-    <title><?php echo htmlspecialchars($item->title); ?></title>
-    <link><?php echo $item->url; ?></link>
-    <description>
-        <?php echo htmlspecialchars($item->object->content); ?>
-    </description>
-    </item>
-<?php } ?>
+// Output RSS feed
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+echo "<rss version=\"2.00\">\n";
+echo "<channel>\n";
+echo "  <title>".htmlspecialchars($gplus->title)."</title>\n";
+echo "  <link>http://plus.google.com/$googlePlusID</link>\n";
+echo "  <description></description>\n";
+echo "  <pubDate>".$gplus->updated."</pubDate>\n";
+echo "  <lastBuildDate>".$gplus->updated."</lastBuildDate>\n";
+foreach($gplus->items as $item) {
+    echo "  <item>\n";
+    echo "      <title>".htmlspecialchars($item->title)."</title>\n";
+    echo "      <link>".htmlspecialchars($item->url)."</link>\n";
+    echo "      <guid>".htmlspecialchars($item->id)."</guid>\n";
+    echo "      <pubDate>".$item->updated."</pubDate>\n";
 
-</channel>
-</rss>
+    // we might add a source quote from attachments
+    $desc = $item->object->content;
+
+    if(isset($item->object->attachments)) foreach($item->object->attachments as $attach){
+        if($attach->objectType == 'article'){
+            $desc .= '<blockquote>';
+            $desc .= '<b><a href="'.$attach->url.'">'.$attach->displayName.'</a></b><br /><br />';
+            $desc .= $attach->content;
+            $desc .= '</blockquote>';
+        }elseif($attach->objectType == 'photo'){
+            echo "        <enclosure url=\"".htmlspecialchars($attach->image->url)."\" type=\"".$attach->image->type."\" />\n";
+        }// FIXME what other attachement type need to be supported?
+    }
+
+    echo "      <description>".htmlspecialchars($desc)."</description>\n";
+
+    echo "  </item>\n";
+}
+echo "</channel>\n";
+echo "</rss>\n";
