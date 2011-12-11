aeae497906f94457c951f9a28d38dbf3c0e23b42
Nils Hitze <nhitze@gmail.com>
2011-12-11 11:33:51 +0100

    added comments and stuff

--- gplus2rss.php
+++ gplus2rss.php
@@ -1,26 +1,35 @@
 <?php
-error_reporting(E_ALL);
+// your cache file, make sure it's writable
 $cacheFile = './cache/gplus-cache';
+// initialise cached flag
 $cacheLoaded = false;
-exit;
+// 30 minutes Cache Time
+$cacheTime = 1800;
+// a (in this case my) Google Plus ID
+$googlePlusID = '113799277735885972934';
+
+// no cachefile? generate it
 if(!file_exists($cacheFile)) {
 	$handle = fopen($cacheFile, 'a');
 	fclose($handle);
 	if(!$handle) {
-		echo 'cache failed';
+		die('cache creation failed');
 	}
+// cache file? use it	
 } else {
-	if((time() - filemtime($cacheFile) < 1800)
+	// cache validation
+	if((time() - filemtime($cacheFile) < $cacheTime)
 	&& filesize($cacheFile)>0) {
 		$content = file_get_contents($cacheFile);
 		$cacheLoaded = true;
 	}
 }
 
+// no cache, reload
 if($cacheLoaded == false) {
 
 	$key = 'YOUR-API-KEY';
-	$url = 'https://www.googleapis.com/plus/v1/people/113799277735885972934/activities/public?alt=json&pp=1&key=';
+	$url = 'https://www.googleapis.com/plus/v1/people/' . $googlePlusID . '/activities/public?alt=json&pp=1&key=';
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url.$key);
@@ -28,22 +37,22 @@ if($cacheLoaded == false) {
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	$content = curl_exec($ch);
 	curl_close($ch);
-//	file_put_contents($cacheFile, $content);
+	file_put_contents($cacheFile, $content);
 }
 $gplus = json_decode($content);
 ?>
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <rss version="2.00">
 <channel>
-<title>Mein GPlus</title>
-<link>http://plus.google.com</link>
+<title>Google+ Feed:</title>
+<link>http://plus.google.com/<?php echo $googlePlusID ?></link>
 <description></description>
 <language>de-de</language>
 <?php
 require_once 'simplepie.inc';
 
 $feed = new SimplePie();
-$feed->set_feed_url('http://silberkind.de/gplus.php');
+$feed->set_feed_url('http://yourdomainhere.tld/gplus2rss.php');
 $feed->enable_cache(false);
 $feed->init();
 
